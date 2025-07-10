82dc8518064253e7e46125c11aa68fd17875f250
essentrad <oluwadamilola293@gmail.com>
2025-07-09 20:22:48 -0700

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
-    const { name, email, message } = JSON.parse(event.body); // Add message
+    const { name, email, message } = JSON.parse(event.body); 
     return {
       statusCode: 200,
       body: JSON.stringify({ message: Thanks, ${name}! We received your email: ${email} and message: ${message} }),
