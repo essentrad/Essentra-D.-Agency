e8a8bbd0f8fd4915659da37281bddeabe94fc76e
essentrad <oluwadamilola293@gmail.com>
2025-07-09 19:47:19 -0700

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'POST') {
+    const { name, email, message } = JSON.parse(event.body); // Add message
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: Thanks, ${name}! We received your email: ${email} and message: ${message} }),
+    };
+  }
+  return {
+    statusCode: 400,
+    body: JSON.stringify({ message: 'Only POST requests are allowed' }),
+  };
+};
